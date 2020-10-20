0f049ffa99720e7cc69345ad43867f24d7845f97
Gerrit Degenhardt <gerrit@degenhardt.be>
2020-10-20 11:10:36 +0200

    Make skeleton

--- src/App.js
+++ src/App.js
@@ -3,42 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import Header from "./Header";
 import Form from "./Form";
+import Section from "./Section";
 import Footer from "./Footer";
-
-const ListTitle = () => {
-    return (
-        <h3 className="text-2xl font-bold leading-7 text-gray-900" >Todos</h3 >
-    )
-}
-
-const List = () => {
-    return (
-        <ul >
-            <li >
-                <label className="md:w-2/3 block text-gray-500 font-bold" >
-                    <input className="mr-2 leading-tight" type="checkbox" />
-                    <span className="text-sm" >Teach Symfony</span >
-                </label >
-            </li >
-            <li >
-                <label className="md:w-2/3 block text-gray-500 font-bold" >
-                    <input className="mr-2 leading-tight" type="checkbox" />
-                    <span className="text-sm" >Send newsletter</span >
-                </label >
-            </li >
-        </ul >
-    )
-}
-
-const Section = () => {
-    return (
-        <section className="p-6 " >
-            <ListTitle />
-            <List />
-        </section >
-    )
-}
-
 
 function App() {
     return (
